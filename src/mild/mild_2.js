9cc100b31941916ec00fe18739c85abfae9890f7
zeyuz18 <zeyuz18@live.unc.edu>
2021-11-28 21:46:31 -0500

    mild2

--- src/mild/mild_2.js
+++ src/mild/mild_2.js
@@ -6,7 +6,7 @@
  * returns: { type: 'number', value: 4 }
  */
 export function identifyVariable(variable) {
-
+   return {type: typeof(variable), value: variable};
 }
 
 
@@ -24,7 +24,11 @@ export function identifyVariable(variable) {
 
  */
 export function identifyArray(array) {
-
+   let a = [];
+   for (let i = 0; i < a.length; i++) {
+      a.push(identifyVariable(array[i]))
+   }
+   return a;
 }
 
 /**
@@ -44,7 +48,7 @@ export function identifyArray(array) {
  obj now does not contain the `password` field
  */
 export function removeKey(object, key) {
-
+   delete object[key];
 }
 
 /**
@@ -64,7 +68,9 @@ export function removeKey(object, key) {
  If only `removeKeyNonDestructive` was called, nothing would have changed.
  */
 export function removeKeyNonDestructive(object, key) {
-
+   let res = JSON.parse(JSON.stringify(object));
+   removeKey(res,key);
+   return res;
 }
 
 /**
@@ -89,5 +95,9 @@ export function removeKeyNonDestructive(object, key) {
  * @return {*} The object with its keys removed.
  */
 export function removeKeys(object, keyList) {
-
+   let res = JSON.parse(JSON.stringify(object));
+   for(let i = 0; i < keyList.length; i++) {
+      removeKey(res,keyList[i])
+   }
+   return res;
 }
